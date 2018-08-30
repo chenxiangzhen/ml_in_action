efee09ecc37fd2a0ab6760ec242f7c5ca9c5d6d1
chenxiangzhen <2839097618@qq.com>
2018-08-30 11:40:26 +0800

    decision tree

--- 02knn/knn.py
+++ 02knn/knn.py
@@ -1,10 +1,12 @@
 from numpy import *
 import operator
 
+
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
+
 
 def classify0(intX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
@@ -12,13 +14,14 @@ def classify0(intX, dataSet, labels, k):
     sqDiffMat = diffMat ** 2
     sqDistance = sqDiffMat.sum(axis=1)
     distances = sqDistance ** 0.5
-    sortedDisIndicies = distances.argsort()  # 从小到大排列
+    sortedDisIndicies = distances.argsort()  # 从小到大排列，返回index
     classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDisIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
 
 def test1():
     """
@@ -28,5 +31,6 @@ def test1():
     print('group:'+str(group))
     print('labels:'+str(labels))
     print(classify0([0.1, 0.1], group, labels, 3))
+
 
 test1()
