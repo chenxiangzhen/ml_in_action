85bfefb3e8a7ea97061dac0e608c1948c0c9dfb8
chenxiangzhen <2839097618@qq.com>
2018-10-14 18:15:21 +0800

    linear_regression

--- 01linear_regression/lr_model.py
+++ 01linear_regression/lr_model.py
@@ -23,6 +23,7 @@ class lr_model():
 
     def linear_loss(self, X, y, w, b):
         num_train = X.shape[0]
+        num_feature = X.shape[1]
 
         y_hat = np.dot(X, w) + b
         loss = np.sum((y_hat - y) ** 2) / num_train
@@ -31,7 +32,7 @@ class lr_model():
         return y_hat, loss, dw, db
 
     def linear_train(self, X, y, learning_rate, epochs):
-        w, b = self.initialize_params(X.shape[0])
+        w, b = self.initialize_params(X.shape[1])
         for i in range(1, epochs):
             y_hat, loss, dw, db = self.linear_loss(X, y, w, b)
             w += -learning_rate * dw
@@ -43,12 +44,10 @@ class lr_model():
                 'w': w,
                 'b': b
             }
-
             grads = {
-                'dw':dw,
-                'db':db
+                'dw': dw,
+                'db': db
             }
-
         return loss, params, grads
 
     def predict(self, X, params):
@@ -70,6 +69,7 @@ class lr_model():
             validation = np.array(validation)
             yield train, validation
 
+
 if __name__ == '__main__':
     lr = lr_model()
     data = lr.prepare_data()
@@ -86,5 +86,5 @@ if __name__ == '__main__':
         score = np.mean(loss5)
         print('five kold cross validation score is', score)
         y_pred = lr.predict(X_valid, params)
-        valid_score = np.sum((y_pred - y_valid) ** 2) / len(X_valid)
+        valid_score = np.sum(((y_pred - y_valid) ** 2)) / len(X_valid)
         print('valid score is', valid_score)
